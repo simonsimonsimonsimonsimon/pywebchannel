37e5f1e240144e7cce80515ddc81a3314c9bd541
Simon Weinzierl <s.weinzierl@menlosystems.com>
2023-11-29 16:31:15 +0100

    Added support for creating a menlosystem namespace package

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 import setuptools
+import sys
+sys.path.append("./menlosystems")
 import pywebchannel
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pywebchannel",
+    name="menlosystems-pywebchannel",
     version=pywebchannel.__version__,
     author=pywebchannel.__author__,
     author_email=pywebchannel.__email__,
@@ -13,7 +15,8 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/menlosystems/pywebchannel",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_namespace_packages(where='./', include=['menlosystems.pywebchannel']),
+    #package_dir={'': 'src'},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
